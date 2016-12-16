b7bf64cece2b2f8e8017c762f496fdab643a1558
inphood <prabhaav.bhardwaj@gmail.com>
2016-12-16 11:28:02 -0800

    infra work for sagas, reducers, css, bootstrap, and other helpers

--- react-inphood/src/index.js
+++ react-inphood/src/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import "babel-polyfill"
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import AppContainer from './containers/AppContainer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootReducer from './reducers'
+import rootSaga from './sagas'
+import './index.css';
+
+import sagaMonitor from './sagas/sagaMonitor'
+const sagaMiddleware = createSagaMiddleware({sagaMonitor})
+// const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(rootSaga)
+
+render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('root')
+)
