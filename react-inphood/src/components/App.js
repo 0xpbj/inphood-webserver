1d6c0cf5e1eb9edaa78c4e9640f4c73117f426d0
inphood <prabhaav.bhardwaj@gmail.com>
2016-12-17 17:46:29 -0800

    initial hello world website

--- react-inphood/src/components/App.js
+++ react-inphood/src/components/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import logo from '../data/logo.svg'
-import './App.css'
+import './styles/App.css'
+
+// import 'bootstrap/less/bootstrap.less'
+import './styles/custom-styles.css'
+
+import HeaderNavigation from './HeaderNavigation'
+import Body from './Body'
+import Footer from './Footer'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Money $$.
-        </p>
+        <HeaderNavigation />
+        <Body />
+        <Footer />
       </div>
     )
   }
