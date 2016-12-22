a639153962b19e0dd046c9e005ebd06df864792a
Alex Carreira <aycarrei@gmail.com>
2016-12-22 11:40:02 -0800

    Updated to included edit distance information (must pip install editdistance, see https://pypi.python.org/pypi/editdistance)

--- inphood/nutritionLabel/views.py
+++ inphood/nutritionLabel/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import NutritionInfo
 
+import editdistance
+
 def index(request):
   latestNutritionInfoList = NutritionInfo.objects.order_by('-pub_date')[:5]
   context = { 'latestNutritionInfoList': latestNutritionInfoList }
@@ -25,6 +27,6 @@ def foodDetail(request, food_name):
   matches = NutritionInfo.objects.filter(description__startswith=food_name)
   descriptions = ""
   for match in matches:
-    descriptions += match.description + "<br>"
+    descriptions += "<div style=\"text-indent: 5em;\">" + match.description + " (" + str(editdistance.eval(match.description, food_name)) + ")<br></div>"
 
-  return HttpResponse("Nutrition matches for term " + food_name + " = " + str(len(matches)) + ". <br>" + descriptions)
+  return HttpResponse(str(len(matches)) + " nutrition matches for term " + food_name + ": <br><br>" + descriptions)
